4333bc2ba989677c389bc13839624c6a19ad3a75
Michal Simik <michal.simik@gmail.com>
2022-05-12 00:11:58 +0200

    feat: shrink view on keyboard appearance

--- GreenCodeAssignment/Scenes/ResultForm/ResultFormViewController.swift
+++ GreenCodeAssignment/Scenes/ResultForm/ResultFormViewController.swift
@@ -9,18 +9,27 @@ import UIKit
 
 class ResultFormViewController: UIViewController {
     private let tableView = UITableView()
+    private let applyButton = UIButton()
+    private var stackView: UIStackView!
 
     var viewModel: ResultFormViewModelInput?
     let dataSource = ResultFormTableViewDataSource()
 
+    var bottomConstraint: NSLayoutConstraint?
+
     override func loadView() {
         super.loadView()
         setLayout()
+        registerKeyboardNotifications()
+
         title = "ResultForm.title".localized
 
         tableView.register(ResultFormTableViewCell.self, forCellReuseIdentifier: String(describing: ResultFormTableViewCell.self))
         tableView.dataSource = dataSource
         tableView.allowsSelection = false
+
+        bottomConstraint = stackView.bottomAnchor.constraint(equalTo: view.bottomAnchor)
+        bottomConstraint?.isActive = true
     }
 
     override func viewDidLoad() {
@@ -28,23 +37,51 @@ class ResultFormViewController: UIViewController {
         viewModel?.viewDidLoad()
     }
 
+    func registerKeyboardNotifications() {
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillShow), name: UIResponder.keyboardWillShowNotification, object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillHide), name: UIResponder.keyboardWillHideNotification, object: nil)
+    }
+
     func setLayout() {
         view.backgroundColor = .systemBackground
 
-        tableView.translatesAutoresizingMaskIntoConstraints = false
-        view.addSubview(tableView)
+        applyButton.setTitle("ResultForm.applyButton".localized, for: .normal)
+        applyButton.setTitleColor(.systemGray, for: .normal)
+
+        stackView = UIStackView(arrangedSubviews: [tableView, applyButton])
+        stackView.axis = .vertical
+        stackView.distribution = .fill
+        stackView.alignment = UIStackView.Alignment.center
+        stackView.spacing = 5
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+
+        view.addSubview(stackView)
 
         NSLayoutConstraint.activate([
-            tableView.topAnchor.constraint(equalTo: view.topAnchor),
-            tableView.bottomAnchor.constraint(equalTo: view.bottomAnchor),
-            tableView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
-            tableView.trailingAnchor.constraint(equalTo: view.trailingAnchor)
+            applyButton.heightAnchor.constraint(equalToConstant: 50),
+            applyButton.widthAnchor.constraint(equalTo: stackView.widthAnchor),
+            tableView.widthAnchor.constraint(equalTo: stackView.widthAnchor),
+
+            stackView.topAnchor.constraint(equalTo: view.topAnchor),
+            stackView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            stackView.trailingAnchor.constraint(equalTo: view.trailingAnchor)
         ])
 
         let addBarButtonItem = UIBarButtonItem(barButtonSystemItem: .cancel, target: self, action: #selector(cancelButtonClicked))
         let confirmBarButtonItem = UIBarButtonItem(barButtonSystemItem: .save, target: self, action: #selector(confirmButtonClicked))
         self.navigationItem.leftBarButtonItem = addBarButtonItem
         self.navigationItem.rightBarButtonItem = confirmBarButtonItem
+    }
+
+    @objc func keyboardWillShow(sender: NSNotification) {
+        if let info = sender.userInfo, let keyboardFrameEndUserInfoKey = info[UIResponder.keyboardFrameEndUserInfoKey] as? NSValue {
+            let keyboardSize = keyboardFrameEndUserInfoKey.cgRectValue.height
+            bottomConstraint?.constant = -keyboardSize
+        }
+    }
+
+    @objc func keyboardWillHide(sender: NSNotification) {
+        bottomConstraint?.constant = 0
     }
 
     @objc func cancelButtonClicked() {
